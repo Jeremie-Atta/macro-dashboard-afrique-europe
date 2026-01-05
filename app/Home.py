f8e4f6a4b5f7ad742ab5ec883522b5bcfce51f1d
‘’Jeremie-Atta’’ <attajeremiek@gmail.com>
2026-01-05 22:32:35 +0000

    Update 1 Dashbord Macro

--- app/Home.py
+++ app/Home.py
@@ -20,3 +20,15 @@ Ce dashboard s’appuie sur les données **World Bank Open Data** (2000–2024) 
 Utilise le menu à gauche pour naviguer.
 """
 )
+
+st.divider()
+
+st.markdown(
+    """
+    <div style="text-align:center; color: #6b7280; font-size: 0.9em;">
+        Réalisé par <strong>Atta Jérémie KOUAME</strong> · Data Analyst / Économiste<br>
+        © 2026
+    </div>
+    """,
+    unsafe_allow_html=True
+)
